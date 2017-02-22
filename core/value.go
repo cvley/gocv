d91931c2f55925117169111cf2c77cd3c686b2a5
cvley <ley@hackcv.com>
2017-02-22 23:12:41 +0800

    refactor value.go: set Char/Int/Float type and conversions

--- core/value.go
+++ core/value.go
@@ -1,116 +1,173 @@
 package core
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Value interface is used to convert value to desired type
 type Value interface {
-	Int() int
-	Int64() int64
-	Uint() uint
-	Uint64() uint64
-	Float64() float64
+	Char() Char
+	Int() Int
+	Float() Float
+	String() string
+	Channels() int
 }
 
-type Int int
-type Int64 int64
-type Uint uint
-type Uint64 uint64
-type Float64 float64
+type Char struct {
+	data     []int
+	channels int
+}
 
-func (i Int) Int() int {
+type Int struct {
+	data     []int
+	channels int
+}
+type Float struct {
+	data     []float64
+	channels int
+}
+
+func (i Char) Char() Char {
+	return i
+}
+
+func (i Char) Int() Int {
+	intv := Char{
+		data:     make([]int, len(i.data)),
+		channels: i.channels,
+	}
+	for i, v := range i.data {
+		intv.data[i] = int(v)
+	}
+	return intv
+
+}
+func (i Char) Float() Float {
+	float := Float{
+		data:     make([]float64, len(i.data)),
+		channels: i.channels,
+	}
+
+	for i, v := range i.data {
+		float.data[i] = float64(v)
+	}
+	return float
+}
+
+func (i Char) String() string {
+	result := make([]string, i.channels)
+	for i, v := range i.data {
+		result[i] = fmt.Sprintf("%d", v)
+	}
+
+	return "[" + strings.Join(result, ",") + "]"
+}
+
+func (i Char) Channels() int {
+	return i.channels
+}
+
+func (i Int) Char() Char {
+	char := Char{
+		data:     make([]int, len(i.data)),
+		channels: i.channels,
+	}
+	for i, v := range i.data {
+		char.data[i] = char(v)
+	}
+	return char
+}
+
+func (i Int) Int() Int {
+	return i
+}
+
+func (i Int) Float() Float {
+	float := Float{
+		data:     make([]float64, len(i.data)),
+		channels: i.channels,
+	}
+
+	for i, v := range i.data {
+		float.data[i] = float64(v)
+	}
+	return float
+}
+
+func (i Int) String() string {
+	result := make([]string, i.channels)
+	for i, v := range i.data {
+		result[i] = fmt.Sprintf("%d", v)
+	}
+
+	return "[" + strings.Join(result, ",") + "]"
+}
+
+func (i Int) Channels() int {
+	return i.channels
+}
+
+func (i Float) Char() Char {
+	char := Char{
+		data:     make([]int, len(i.data)),
+		channels: i.channels,
+	}
+	for i, v := range i.data {
+		char.data[i] = char(v)
+	}
+	return char
+}
+
+func (i Float) Int() Int {
 	return int(i)
 }
 
-func (i Int) Int64() int64 {
-	return int64(i)
+func (i Float) Float() Float {
+	float := Char{
+		data:     make([]float64, len(i.data)),
+		channels: i.channels,
+	}
+	for i, v := range i.data {
+		float.data[i] = float64(v)
+	}
+	return float
 }
 
-func (i Int) Uint() uint {
-	return uint(i)
+func (i Float) String() string {
+	result := make([]string, i.channels)
+	for i, v := range i.data {
+		result[i] = fmt.Sprintf("%.4f", v)
+	}
+
+	return "[" + strings.Join(result, ",") + "]"
 }
 
-func (i Int) Uint64() uint64 {
-	return uint64(i)
+func (i Float) Channels() int {
+	return i.channels
 }
 
-func (i Int) Float64() float64 {
-	return float64(i)
-}
+func char(v interface{}) int {
+	switch v.(type) {
+	case int:
+		if v < 0 {
+			return 0
+		}
+		if v > 255 {
+			return 255
+		}
+		return v
 
-func (i Int64) Int() int {
-	return int(i)
-}
+	case float64:
+		if v < 0 {
+			return 0
+		}
+		if v > 255 {
+			return 255
+		}
+		return int(v)
 
-func (i Int64) Int64() int64 {
-	return int64(i)
-}
-
-func (i Int64) Uint() uint {
-	return uint(i)
-}
-
-func (i Int64) Uint64() uint64 {
-	return uint64(i)
-}
-
-func (i Int64) Float64() float64 {
-	return float64(i)
-}
-
-func (i Uint) Int() int {
-	return int(i)
-}
-
-func (i Uint) Int64() int64 {
-	return int64(i)
-}
-
-func (i Uint) Uint() uint {
-	return uint(i)
-}
-
-func (i Uint) Uint64() uint64 {
-	return uint64(i)
-}
-
-func (i Uint) Float64() float64 {
-	return float64(i)
-}
-
-func (i Uint64) Int() int {
-	return int(i)
-}
-
-func (i Uint64) Int64() int64 {
-	return int64(i)
-}
-
-func (i Uint64) Uint() uint {
-	return uint(i)
-}
-
-func (i Uint64) Uint64() uint64 {
-	return uint64(i)
-}
-
-func (i Uint64) Float64() float64 {
-	return float64(i)
-}
-
-func (i Float64) Int() int {
-	return int(i)
-}
-
-func (i Float64) Int64() int64 {
-	return int64(i)
-}
-
-func (i Float64) Uint() uint {
-	return uint(i)
-}
-
-func (i Float64) Uint64() uint64 {
-	return uint64(i)
-}
-
-func (i Float64) Float64() float64 {
-	return float64(i)
+	default:
+		panic("char func not support value type")
+	}
 }
